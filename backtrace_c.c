601d45b45af07ef06b995186ec99bab3959bcb14
Jeff Hammond <jeff.science@gmail.com>
2024-08-28 12:54:11 +0300

    cleanup
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- backtrace_c.c
+++ backtrace_c.c
@@ -3,8 +3,19 @@
 
 #include <execinfo.h>
 
+#define MAX_DEPTH 100
+
 void c_backtrace(void)
 {
+    void* pointers[MAX_DEPTH];
+    const int depth = backtrace(pointers, MAX_DEPTH);
 
-
+    char** symbols = NULL;
+    symbols = backtrace_symbols(pointers, depth);
+    if (symbols != NULL) {
+        for (int i=0; i<depth; i++) {
+            fprintf(stderr, "%p %s\n", pointers[i], symbols[i]);
+        }
+    }
+    free(symbols);
 }
