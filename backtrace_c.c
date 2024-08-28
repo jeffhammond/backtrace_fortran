1170c5a2e80c939d675a8669806b7fd0b2f157e1
Jeff Hammond <jeff.science@gmail.com>
2024-08-28 12:44:18 +0300

    more
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

---
+++ backtrace_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <execinfo.h>
+
+void c_backtrace(void)
+{
+
+
+}
--- backtrace_f.F90
+++ backtrace_f.F90
@@ -1,9 +1,22 @@
 module backtrace_m
 
+    interface
+        pure subroutine c_backtrace() bind(C,name="c_backtrace")
+            implicit none
+        end subroutine c_backtrace
+    end interface
+
+    contains
+
+    subroutine fortran_backtrace
+        implicit none
+        call c_backtrace
+    end subroutine fortran_backtrace
+
 end module backtrace_m
 
 subroutine backtrace
     use backtrace_m
     implicit none
-    
+    call fortran_backtrace    
 end subroutine backtrace
